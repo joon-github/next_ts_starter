986239bdba03df84dd9aec2dca9e6176c7dca955
beomJoon <ehdfkd2000@naver.com>
2023-02-12 21:53:00 +0900

    [Frontend_starter] 프로젝트 세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Test = styled.div`
+  background-color: green;
+  color:red;
+`
+
+export default function Home() {
+  return (
+    <>
+      <Test>root</Test>
+    </>
+  )
+}
